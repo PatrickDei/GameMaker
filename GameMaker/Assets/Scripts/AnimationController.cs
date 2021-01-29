b419a20fbdbebd2ebb9d0f0f553b7ce3dab7ff41
Patrick Deisinger <patrick.deis@gmail.com>
2021-01-30 00:16:35 +0100

    Initial commit, menu animated

---
+++ GameMaker/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+
+    public static void AnimateObject(GameObject obj)
+    {
+        if (obj.tag == "MenuItem")
+            obj.GetComponent<Animation>().Play();
+    }
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Game Maker
+
+<description>
+This app will let you make your own customisable game
+
+## Branching rules
+We will branch out for features and fixes instead of people (easier to maintain)
+Also the artists will have their "asset-entry" branch
