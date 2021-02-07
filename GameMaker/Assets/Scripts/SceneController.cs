2f335bf102b71c246a2b6f5dcff24c440d53d6fa
Patrick Deisinger <patrick.deis@gmail.com>
2021-02-07 12:23:27 +0100

    Scene adjustments, .json files

--- GameMaker/Assets/Scripts/SceneController.cs
+++ GameMaker/Assets/Scripts/SceneController.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
-    
+    static void OnSceneLoad(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
 }
