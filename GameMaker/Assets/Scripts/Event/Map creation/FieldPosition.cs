80ec07373ca24ef575e20ebacf219e23cde4fa59
Patrick Deisinger <patrick.deis@gmail.com>
2021-08-29 00:50:09 +0200

    Level creation, object follows cursor

---
+++ GameMaker/Assets/Scripts/Event/Map creation/FieldPosition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldPosition : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+    [SerializeField] public static GameObject target;
+
+    void Update()
+    {
+        if (target)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                target.transform.position = new Vector3(hit.point.x, target.transform.position.y, hit.point.z);
+            }
+        }
+    }
+}
---
+++ GameMaker/Assets/Scripts/Event/Map creation/MapObjectCreator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapObjectCreator : MonoBehaviour
+{
+
+    public void AddObject()
+    {
+        GameObject newObject = GameObject.CreatePrimitive((gameObject.name == "Cube") ? PrimitiveType.Cube : PrimitiveType.Sphere);
+        newObject.transform.position = GameObject.Find("Plane").transform.position;
+        FieldPosition.target = newObject;
+    }
+}
